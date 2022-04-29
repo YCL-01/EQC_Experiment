3520b41efedab7721e04fd064a742bac77db1569
youngchanlim <youngchanlim@naver.com>
2022-04-30 00:08:54 +0900

    Update FTPActivity.java

--- app/src/main/java/com/example/expapp/FTPActivity.java
+++ app/src/main/java/com/example/expapp/FTPActivity.java
@@ -16,10 +16,10 @@ import it.sauronsoftware.ftp4j.FTPIllegalReplyException;
 
 public class FTPActivity extends Activity{
 
-    static final String FTP_HOST = "115.85.180.227";
-    static final String FTP_USER = "ftpuser";
-    static final String FTP_PASS = "wings";
-    static final int FTP_PORT = 10050;
+    static final String FTP_HOST = 
+    static final String FTP_USER = 
+    static final String FTP_PASS = 
+    static final int FTP_PORT = 
 
     String TAG="FTPLog";
 
